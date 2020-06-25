f5e922d2f4a110d1471f041f85d152a81df079d7
unknown <nandhinimadan20@gmail.com>
2020-06-25 16:32:00 -0700

    Completed

--- src/componts/Photocard.js
+++ src/componts/Photocard.js
@@ -1,9 +1,12 @@
 import React from "react";
-const Photocard=({date1 ,url})=>{
+const Photocard=({date1 ,url,explaination,title})=>{
     return(
  <div>
-<p>{date1}</p>
-<img src={url}></img>
+            <h1>{title}</h1>
+            <p>Date: {date1}</p>
+            <img src={url} width='400px' height='400px'></img>
+            <p>explaination: {explaination}</p>
+            
 </div>
     );
 };
--- src/componts/Photolist.js
+++ src/componts/Photolist.js
@@ -6,7 +6,7 @@ export default function Photolist(){
     const [Photos,SetPhotos]=useState([]);
     useEffect(()=>{
         axios
-        .get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY`)
+        .get(`https://api.nasa.gov/planetary/apod?api_key=huB3roxjgCMiWUbgRWs8ZQ31HJ8nimqG2qrTrezg`)
         .then(res=>{
             console.log("Result of nasa",res);
             SetPhotos(res.data);
@@ -21,7 +21,7 @@ export default function Photolist(){
     return(
 
         <div>
-          <Photocard key={Photos.id} date1={Photos.date} url={Photos.hdurl}/>
+          <Photocard key={Photos.id} date1={Photos.date} url={Photos.hdurl} explaination={Photos.explanation} title={Photos.title}/>
         </div>
     );
 
